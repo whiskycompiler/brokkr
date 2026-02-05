29fea1bd0bec70d21b250226fb7aee04c70720e1
whiskycompiler <54137048+whiskycompiler@users.noreply.github.com>
2026-02-05 09:19:24 +0900

    add efcore adapter for SQL; change to slnx

---
+++ src/Brokkr.DDD.EFCore/BaseEfRepository.cs
@@ -0,0 +1,52 @@
+﻿using Brokkr.DDD.UoW;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Brokkr.DDD.EFCore;
+
+/// <summary>
+/// Base repository class to adapt EF Core repositories to the Brokkr.DDD interfaces.
+/// </summary>
+/// <typeparam name="TContext">The type of the Entity Framework <see cref="DbContext"/> used by this repository.</typeparam>
+/// <typeparam name="TEntity">The type of the entity that the repository operates on.</typeparam>
+public abstract class BaseEfRepository<TContext, TEntity> : IStandaloneRepository<TEntity>
+    where TEntity : class
+    where TContext : DbContext
+{
+    /// <summary>
+    /// EF Core <see cref="DbContext"/> used by this repository to interact with the database.
+    /// </summary>
+    protected TContext Context { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BaseEfRepository{T1,T2}"/> class.
+    /// </summary>
+    protected BaseEfRepository(TContext context)
+    {
+        Context = context;
+    }
+
+    /// <inheritdoc/>
+    public void AddEntity(TEntity entity)
+    {
+        Context.Set<TEntity>().Add(entity);
+    }
+
+    /// <inheritdoc/>
+    public void UpdateEntity(TEntity entity)
+    {
+        Context.Set<TEntity>().Update(entity);
+    }
+
+    /// <inheritdoc/>
+    public void RemoveEntity(TEntity entity)
+    {
+        Context.Set<TEntity>().Remove(entity);
+    }
+
+    /// <inheritdoc/>
+    public virtual Task SaveTrackedChanges(CancellationToken cancellationToken = default)
+    {
+        return Context.SaveChangesAsync(cancellationToken);
+    }
+}
---
+++ src/Brokkr.DDD.EFCore/README.md
@@ -0,0 +1,3 @@
+# Brokkr.DDD.EFCore
+
+Components to integrate EF Core with the Brokkr DDD framework.
